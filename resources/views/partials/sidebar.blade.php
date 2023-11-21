f560f645c30acc71140e698979791c99539232d3
ChecilonaZaneth <checilonazaneth@gmail.com>
2023-11-21 16:20:48 +0700

    first commit

---
+++ resources/views/partials/sidebar.blade.php
@@ -0,0 +1,49 @@
+<aside class="menu-sidebar d-none d-lg-block">
+<div class="logo">
+    <a href="#">
+        <img src="{{asset('admin/images//icon/logo.png')}}" alt="Cool Admin" />
+    </a>
+</div>
+<div class="menu-sidebar__content js-scrollbar1">
+    <nav class="navbar-sidebar">
+        <ul class="list-unstyled navbar__list">
+            <li class="active has-sub">
+                <a class="js-arrow" href="#">
+                    <i class="fas fa-tachometer-alt"></i>Dashboard</a>
+                <ul class="list-unstyled navbar__sub-list js-sub-list">
+                    <li>
+                        <a href="index.html">Dashboard 1</a>
+                    </li>
+                    <li>
+                        <a href="index2.html">Dashboard 2</a>
+                    </li>
+                    <li>
+                        <a href="index3.html">Dashboard 3</a>
+                    </li>
+                    <li>
+                        <a href="index4.html">Dashboard 4</a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <a href="{{ route ('contacts.index')}}">
+                    <i class="fas fa-chart-bar"></i>Data Contact Us</a>
+            </li>
+            <li class="has-sub">
+                <a class="js-arrow" href="#">
+                <ul class="list-unstyled navbar__sub-list js-sub-list">
+                    <li>
+                        <a href="login.html">Login</a>
+                    </li>
+                    <li>
+                        <a href="register.html">Register</a>
+                    </li>
+                    <li>
+                        <a href="forget-pass.html">Forget Password</a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </nav>
+</div>
+</aside>
